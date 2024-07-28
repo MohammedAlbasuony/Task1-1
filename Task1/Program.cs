05a8de460a62f4df2f810cf29ab9f283ede7a616
Mohammed Albasuony <Mido.beso.hosny@gmail.com>
2024-07-28 18:18:31 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Employee name :  ");
+            string EmpName = Console.ReadLine();
+            Console.Write("Enter Employee ID :  ");
+            int EmpID = int.Parse(Console.ReadLine());
+
+            int EmpAge;
+            do
+            {
+                Console.Write("Enter student Age :  ");
+                EmpAge = int.Parse(Console.ReadLine());
+
+            } while (EmpAge < 0);
+            int Salary;
+            do
+            {
+                Console.Write("Enter Employee Salary :  ");
+                Salary = int.Parse(Console.ReadLine());
+            } while (Salary < 30000);
+            Console.Write($"Your name is : {EmpName} \n  Your Age is : {EmpAge} \n  Your ID is : {EmpID}  \n Your Salary is : {Salary} ");
+
+        }
+    }
+}
