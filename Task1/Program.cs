af72f1c81f26b43e4ccf33f588e1bf4ec985d97d
Mohammed Albasuony <Mido.beso.hosny@gmail.com>
2024-07-28 18:24:54 +0300

    Cleaning code

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,26 +4,29 @@
     {
         static void Main(string[] args)
         {
+            int EmpAge;
+            int Salary;
+
             Console.Write("Enter Employee name :  ");
             string EmpName = Console.ReadLine();
+
             Console.Write("Enter Employee ID :  ");
             int EmpID = int.Parse(Console.ReadLine());
 
-            int EmpAge;
             do
             {
                 Console.Write("Enter student Age :  ");
                 EmpAge = int.Parse(Console.ReadLine());
 
             } while (EmpAge < 0);
-            int Salary;
+
             do
             {
                 Console.Write("Enter Employee Salary :  ");
                 Salary = int.Parse(Console.ReadLine());
             } while (Salary < 30000);
-            Console.Write($"Your name is : {EmpName} \n  Your Age is : {EmpAge} \n  Your ID is : {EmpID}  \n Your Salary is : {Salary} ");
 
+            Console.Write($"Your name is : {EmpName} \n  Your Age is : {EmpAge} \n  Your ID is : {EmpID}  \n Your Salary is : {Salary} ");
         }
     }
 }
